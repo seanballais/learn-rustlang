8054b53d3445c7d7f269e563187e007f0661320b
Sean Francis N. Ballais <sfballais123@gmail.com>
2020-01-05 21:59:57 +0800

    :see_no_evil: :fire: Updated the .gitignore file to correct git-ignore the target directory of all projects in the repo. Removed the unintentionally committed target directory files from the previous commit.

--- variables/target/debug/deps/variables-80742407c823f7d9.d
+++
@@ -1,5 +0,0 @@
-/home/seanballais/Code/Learning/learn-rustlang/variables/target/debug/deps/variables-80742407c823f7d9: src/main.rs
-
-/home/seanballais/Code/Learning/learn-rustlang/variables/target/debug/deps/variables-80742407c823f7d9.d: src/main.rs
-
-src/main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-/home/seanballais/Code/Learning/learn-rustlang/variables/target/debug/variables: /home/seanballais/Code/Learning/learn-rustlang/variables/src/main.rs
