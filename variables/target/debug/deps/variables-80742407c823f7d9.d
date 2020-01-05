2bc359484697701cb5515fe413e4333989532154
Sean Francis N. Ballais <sfballais123@gmail.com>
2020-01-05 21:55:36 +0800

    :sparkles: Added small sandbox project for looking at the effects of assigning a value to an immutable value, and trying out shadowing. Copied from Chapter 3, Part 1 of the Rust book.

---
+++ variables/target/debug/deps/variables-80742407c823f7d9.d
@@ -0,0 +1,5 @@
+/home/seanballais/Code/Learning/learn-rustlang/variables/target/debug/deps/variables-80742407c823f7d9: src/main.rs
+
+/home/seanballais/Code/Learning/learn-rustlang/variables/target/debug/deps/variables-80742407c823f7d9.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/seanballais/Code/Learning/learn-rustlang/variables/target/debug/variables: /home/seanballais/Code/Learning/learn-rustlang/variables/src/main.rs
